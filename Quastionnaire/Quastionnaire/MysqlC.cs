6e46e9b56911da624f3b85b5afdb68a00c432003
CostasDev <costask.dev@gmail.com>
2015-12-05 08:40:30 -0800

    ekana commenct

--- Quastionnaire/Quastionnaire/MysqlC.cs
+++ Quastionnaire/Quastionnaire/MysqlC.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 /// Create a class to manage the Mysql connection with the database.
 /// </summary>
 
+//Costas Commenct
+
+
 using MySql.Data.MySqlClient;
 using System.Configuration;
 
