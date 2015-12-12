ea41ae525793964ef0d7f98a383db3ffbf33a5ba
Νικολαος Μπουντουροπουλος <nmpountou@hotmail.com>
2015-12-13 01:58:47 +0200

    Complete validation data. Check empty values,unique username, email. Combine country and city dynamically. Find first avaliabe id for registration. Final function insert into database and make add_user.

--- Sunrise.MultipleChoice/Sunrise.MultipleChoice/Register.xaml.cs
+++ Sunrise.MultipleChoice/Sunrise.MultipleChoice/Register.xaml.cs
@@ -35,45 +35,58 @@ namespace Sunrise.MultipleChoice
             fill_combobox_country();
             //Must sellect country first.
             Registration_comboBox_city.IsEnabled = false;
-            //Sunrise.MultipleChoice.Data.Logger.Setup();
         }
         private void Registration_submit_button_Click(object sender, RoutedEventArgs e)
         {
-            String msg = null;
+            int avaliable = Registration_get_first_avaliabe_id();
+            logger.Debug("First id: "+avaliable);
+            String msg = "";
             Boolean validate = false;
-            //logger.Debug("asdad");
-            Registration_comboBox_city.IsEnabled = true;
             if (check_empty_textboxes())
             {
-                msg += "Empty fields in registration form. ";
-
-                logger.Debug("FIND empty textboxes!");
+                msg += "Empty fields in registration form.\n ";
+                validate = true;
             }
             if (check_validation_email())
             {
-                msg += "Non validate email. ";
-
+                msg += "Non validate email.\n";
+                validate = true;
             }
             if (check_unique_username())
             {
-                msg += "Must select a unique username. ";
-
+                msg += "Must select a unique username.\n";
+                validate = true;
             }
             if (check_unique_email())
             {
-                msg += "Must select a unique email. ";
-            }
-            if(msg==null)
-            {
+                msg += "Must select a unique email.\n";
                 validate = true;
-                //Registration_error_label.
             }
-            else
+            if (check_password_similarity())
+            {
+                msg += "Passwords must be identical.\n";
+                validate = true;
+            }
+            if (check_password_validation())
+            {
+                msg += "PASSWORD Must contain at least one:\n";
+                msg += "lower case letter, one upper case letter\n";
+                msg+= "one digit and one special character.\n";
+                msg += "Valid special characters are –   @#$%^&+=";
+                validate = true;
+            }
+            if (validate == true)
             {
                 Registration_error_label.IsEnabled = true;
                 Registration_error_label.Content = msg;
             }
+            else
+            {
+                Registration_error_label.IsEnabled = false;
+                int id = Registration_get_first_avaliabe_id();
+            }
         }
+
         private void fill_combobox_country()
         {
             //Initialize the Connection, using default values of a user.
@@ -81,7 +94,7 @@ namespace Sunrise.MultipleChoice
             MysqlC init_con = new MysqlC();
             //init_con.initializeConnection();
 
-            MySqlConnection con = new MySqlConnection(init_con.temp_connection_string()); 
+            MySqlConnection con = new MySqlConnection(init_con.temp_connection_string());
             String query = "SELECT * FROM country;";
             MySqlCommand cmd = new MySqlCommand(query, con);
             MySqlDataReader reader;
@@ -108,20 +121,72 @@ namespace Sunrise.MultipleChoice
         public Boolean check_unique_email()
         {
             Boolean validation = false;
+            //Initialize the Connection, using default values of a user.
+            //Create the temp Connection String
+            MysqlC init_con = new MysqlC();
+            //init_con.initializeConnection();
+            MySqlConnection con = new MySqlConnection(init_con.temp_connection_string());
+            String query = "SELECT email FROM user_information WHERE email = \"" + Registration_email_textBox.Text + "\";";
+            MySqlCommand cmd = new MySqlCommand(query, con);
+            MySqlDataReader reader;
+            try
+            {
+                con.Open();
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    validation = true;
+                    logger.Debug("Found email!!!");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error("RETRIVE_MAIL_FAILED: ", ex);
+            }
+            finally
+            {
+                con.Close();
+            }
 
-            return true;
+            return validation;
         }
 
         public Boolean check_unique_username()
         {
             Boolean validation = false;
+            //Initialize the Connection, using default values of a user.
+            //Create the temp Connection String
+            MysqlC init_con = new MysqlC();
+            //init_con.initializeConnection();
+            MySqlConnection con = new MySqlConnection(init_con.temp_connection_string());
+            String query = "SELECT username FROM account WHERE username = \"" + Registration_username_textBox.Text + "\";";
+            MySqlCommand cmd = new MySqlCommand(query, con);
+            MySqlDataReader reader;
+            try
+            {
+                con.Open();
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    validation = true;
+                    logger.Debug("Found username!!!");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error("RETRIVE_USERNAME_FAILED: ", ex);
+            }
+            finally
+            {
+                con.Close();
+            }
 
-            return true;
+            return validation;
         }
 
         private Boolean check_validation_email()
         {
-            Boolean validation = false;
+            Boolean validation = true;
             //Regex for making sure Email is valid
             Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
             Match match = regex.Match(Registration_email_textBox.Text);
@@ -131,81 +196,241 @@ namespace Sunrise.MultipleChoice
             }
             else
             {
-                validation = true;
+                validation = false;
             }
             return validation;
         }
         private Boolean check_empty_textboxes()
         {
-            Boolean validated = true;
+            Boolean validated = false;
             if (Registration_name_textBox.Text.Equals(""))
             {
                 Registration_image_error_name.Visibility = Visibility.Visible;
+                validated = true;
             }
-            else if (Registration_lastname_textBox.Text.Equals(""))
+            else
             {
                 Registration_image_error_name.Visibility = Visibility.Hidden;
-                Registration_image_error_last_name.Visibility = Visibility.Visible;
             }
-            else if (Registration_email_textBox.Text.Equals(""))
+            if (Registration_lastname_textBox.Text.Equals(""))
+            {
+                Registration_image_error_last_name.Visibility = Visibility.Visible;
+                validated = true;
+            }
+            else
             {
                 Registration_image_error_last_name.Visibility = Visibility.Hidden;
-                Registration_image_error_email.Visibility = Visibility.Visible;
             }
-            else if (Registration_comboBox_country.SelectedIndex == -1)
+            if (Registration_email_textBox.Text.Equals(""))
+            {
+                Registration_image_error_email.Visibility = Visibility.Visible;
+                validated = true;
+            }
+            else
             {
                 Registration_image_error_email.Visibility = Visibility.Hidden;
-                Registration_image_error_country.Visibility = Visibility.Visible;
             }
-            else if (Registration_comboBox_city.SelectedIndex == -1)
+            if (Registration_comboBox_country.SelectedIndex == -1)
+            {
+                Registration_image_error_country.Visibility = Visibility.Visible;
+                validated = true;
+            }
+            else
             {
                 Registration_image_error_country.Visibility = Visibility.Hidden;
-                Registration_image_error_city.Visibility = Visibility.Visible;
             }
-            else if (Registration_textBox_street.Text.Equals(""))
+            if (Registration_comboBox_city.SelectedIndex == -1)
+            {
+                Registration_image_error_city.Visibility = Visibility.Visible;
+                validated = true;
+            }
+            else
             {
                 Registration_image_error_city.Visibility = Visibility.Hidden;
-                Registration_image_error_street.Visibility = Visibility.Visible;
             }
-            else if (Registration_username_textBox.Text.Equals(""))
+            if (Registration_textBox_street.Text.Equals(""))
+            {
+                Registration_image_error_street.Visibility = Visibility.Visible;
+                validated = true;
+            }
+            else
             {
                 Registration_image_error_street.Visibility = Visibility.Hidden;
-                Registration_image_error_username.Visibility = Visibility.Visible;
             }
-            else if (Registration_passwordBox.Password.Equals(""))
+            if (Registration_username_textBox.Text.Equals(""))
+            {
+                Registration_image_error_username.Visibility = Visibility.Visible;
+                validated = true;
+            }
+            else
             {
                 Registration_image_error_username.Visibility = Visibility.Hidden;
+            }
+            if (Registration_passwordBox.Password.Equals(""))
+            {
                 Registration_image_error_password.Visibility = Visibility.Visible;
+                validated = true;
             }
             else
             {
                 Registration_image_error_password.Visibility = Visibility.Hidden;
-                validated = false;
-                //Graph of processing.
-                //Login_Process.Visibility = Visibility.Visible;
-                //Graph of errors
-                //Login_image_error_user_name.Visibility = Visibility.Hidden;
-                //Login_image_error_password.Visibility = Visibility.Hidden;
-
+            }
+            //Check the reverse.
+            if (Registration_passwordBox_Copy.Password.Equals(""))
+            {
+                Registration_image_error_password.Visibility = Visibility.Visible;
+            }
+            else if (Registration_passwordBox_Copy.Password.Equals(Registration_passwordBox.Password))
+            {
+                Registration_image_error_password.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                Registration_image_error_password.Visibility = Visibility.Visible;
+                validated = true;
             }
             return validated;
-
         }
+        private Boolean check_password_similarity()
+        {
+            Boolean validate = true;
+            if (Registration_passwordBox_Copy.Password.Equals(Registration_passwordBox.Password))
+            {
+                validate = false;
+            }
+            return validate;
+        }
+        /// <summary>
+        /// Must be at least 6 characters
+        // Must contain at least one one lower case letter, one upper case letter, one digit and one special character
+        // Valid special characters are –   @#$%^&+=
+        /// </summary>
+        /// <returns></returns>
+        private Boolean check_password_validation()
+        {
+            Boolean validation = true;
+            Regex regex = new Regex(@"^.*(?=.{6,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$");
+            Match match = regex.Match(Registration_passwordBox.Password);
+            if (!match.Success)
+            {
+                Registration_image_error_email.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                validation = false;
+            }
 
+            return validation;
+        }
         private void Registration_comboBox_city_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if(Registration_comboBox_country.SelectedIndex != -1)
-            {
 
-            }
         }
-
         private void Registration_comboBox_country_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            String cities;
+            Registration_comboBox_city.Items.Clear();
             if (Registration_comboBox_country.SelectedIndex != -1)
             {
                 Registration_comboBox_city.IsEnabled = true;
+                int country_id = Registration_comboBox_country_getid(Registration_comboBox_country.SelectedItem.ToString());
+                //Initialize the Connection, using default values of a user.
+                //Create the temp Connection String
+                MysqlC init_con = new MysqlC();
+                //init_con.initializeConnection();
+                MySqlConnection con = new MySqlConnection(init_con.temp_connection_string());
+                String query = "SELECT city FROM city WHERE country_id = " + country_id + ";";
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                MySqlDataReader reader;
+                try
+                {
+                    con.Open();
+                    reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        cities = reader.GetString("city");
+                        //logger.Debug("Find cities for country id: " + country_id+" cities: "+cities);
+                        Registration_comboBox_city.Items.Add(cities);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    logger.Error("RETRIVE_CITY_FAILED: ", ex);
+                }
+                finally
+                {
+                    con.Close();
+                }
             }
+        }
+        private int Registration_comboBox_country_getid(string country)
+        {
+            int country_id=0;
+            country = Registration_comboBox_country.SelectedItem.ToString();
+            logger.Debug("RETRIVE_COUNTRY: " + country);
+            //Initialize the Connection, using default values of a user.
+            //Create the temp Connection String
+            MysqlC init_con = new MysqlC();
+            //init_con.initializeConnection();
+            MySqlConnection con = new MySqlConnection(init_con.temp_connection_string());
+            String query = "SELECT id FROM country WHERE country = \"" + country + "\";";
+            MySqlCommand cmd = new MySqlCommand(query, con);
+            MySqlDataReader reader;
+            try
+            {
+                con.Open();
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                     country_id = reader.GetInt16("id");
+                     //Registration_comboBox_country.Items.Add(country_id);
+                     logger.Debug("Find cities for country id: " + country_id);
+                     //Registration_comboBox_country.Items.Add(country_id);
+                }
+            }
+            catch (Exception ex)
+            {
+               logger.Error("RETRIVE_CITY_FAILED: ", ex);
+            }
+            finally
+            {
+                con.Close();     
+            }
+            return country_id;
+        }
+        private int Registration_get_first_avaliabe_id()
+        {
+            int availiable=0;
+            Boolean validation = false;
+            //Initialize the Connection, using default values of a user.
+            //Create the temp Connection String
+            MysqlC init_con = new MysqlC();
+            //init_con.initializeConnection();
+            MySqlConnection con = new MySqlConnection(init_con.temp_connection_string());
+            string query = "SELECT u.id + 1 AS FirstAvailableId " +
+                "FROM account u LEFT JOIN account u1 ON u1.id = u.id + 1 " +
+                "WHERE u1.id IS NULL ORDER BY u.id   LIMIT 0, 1";
+            MySqlCommand cmd = new MySqlCommand(query, con);
+            MySqlDataReader reader;
+            try
+            {
+                con.Open();
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    availiable = reader.GetInt16("FirstAvailableId");
+                    logger.Debug("Found first avaliable id!!!"+availiable);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error("RETRIVE_AVAILIABLE_ID_FAILED: ", ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return availiable;
         }
     }
 }
