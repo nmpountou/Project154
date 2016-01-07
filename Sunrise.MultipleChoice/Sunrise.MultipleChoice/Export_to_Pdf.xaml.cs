7da425cedc3ee7431580d526dc72838980e6efca
Νικολαος Μπουντουροπουλος <nmpountou@hotmail.com>
2016-01-07 17:19:14 +0200

    Finish the header the date and the name. Need to retrive the questions from databse. .

--- Sunrise.MultipleChoice/Sunrise.MultipleChoice/Export_to_Pdf.xaml.cs
+++ Sunrise.MultipleChoice/Sunrise.MultipleChoice/Export_to_Pdf.xaml.cs
@@ -31,8 +31,12 @@ namespace Sunrise.MultipleChoice
         PdfPage page;
         XGraphics gfx;
         XFont font;
+        XFont header_font;
         string filename;
         int questionaire;
+        //Where the header finishes
+        double x=0;
+        double y=0;
         public Export_to_Pdf()
         {
             InitializeComponent();
@@ -42,6 +46,7 @@ namespace Sunrise.MultipleChoice
             gfx = XGraphics.FromPdfPage(page);
             //Set by default values.
             font = new XFont("Arial", 12, XFontStyle.Regular);
+            header_font = new XFont("Arial",12, XFontStyle.Regular);
             page.Size = PageSize.A4;
             page.Orientation = PageOrientation.Portrait;
         }
@@ -54,24 +59,21 @@ namespace Sunrise.MultipleChoice
         {
             set_title_subject_author_keyword();
             set_font();
+            set_header_font();
             page_size();
             page_orientation();
             header();
             insert_questions("Hello, World!");
             //gfx.DrawString("Hello, World!", font, XBrushes.Black,new XRect(5, 5, page.Width, page.Height),XStringFormats.TopLeft);
+            initialize_parameters_file();
 
-            if (Export_to_pdf__textbox_filename.Text=="")
-            {
-                filename = "HelloWorld.pdf";
-            }
-            else
-            {
-                filename = Export_to_pdf__textbox_filename.Text+".pdf";
-            }
-            
-            document.Save(Export_to_pdf__select_folder_save.Text+filename);
+            document.Save(Export_to_pdf__select_folder_save.Text + filename);
+
             // ...and start a viewer.
-           Process.Start(Export_to_pdf__select_folder_save.Text+filename);
+            Process.Start(Export_to_pdf__select_folder_save.Text+filename);
+            //in case we want to calculate the pdf again.
+            //When process start call it disposes the object. 
+            dispose_objects_load();
 
         }
 
@@ -126,6 +128,33 @@ namespace Sunrise.MultipleChoice
                 font = new XFont(Export_to_pdf_combobox_font.Text, Convert.ToDouble(Export_to_pdf_comobox_fontsize.Text), XFontStyle.Underline);
             }
         }
+        private void set_header_font()
+        {
+            if (header_fontstyle_bold.IsSelected)
+            {
+                header_font = new XFont(Export_to_pdf_Header_combobox_font.Text, Convert.ToDouble(Export_to_pdf_Header_comobox_fontsize.Text), XFontStyle.Bold);
+            }
+            else if (header_fontstyle_bolditalic.IsSelected) 
+            {
+                header_font = new XFont(Export_to_pdf_Header_combobox_font.Text, Convert.ToDouble(Export_to_pdf_Header_comobox_fontsize.Text), XFontStyle.BoldItalic);
+            }
+            else if (header_fontstyle_italic.IsSelected)
+            {
+                header_font = new XFont(Export_to_pdf_Header_combobox_font.Text, Convert.ToDouble(Export_to_pdf_Header_comobox_fontsize.Text), XFontStyle.Italic);
+            }
+            else if (header_fontstyle_regural.IsSelected)
+            {
+                header_font = new XFont(Export_to_pdf_Header_combobox_font.Text, Convert.ToDouble(Export_to_pdf_Header_comobox_fontsize.Text), XFontStyle.Regular);
+            }
+            else if (header_fontstyle_strikeout.IsSelected)
+            {
+                header_font = new XFont(Export_to_pdf_Header_combobox_font.Text, Convert.ToDouble(Export_to_pdf_Header_comobox_fontsize.Text), XFontStyle.Strikeout);
+            }
+            else if (header_fontstyle_underline.IsSelected)
+            {
+                header_font = new XFont(Export_to_pdf_Header_combobox_font.Text, Convert.ToDouble(Export_to_pdf_Header_comobox_fontsize.Text), XFontStyle.Underline);
+            }
+        }
         private void set_title_subject_author_keyword()
         {
             document.Info.Title = Export_to_pdf__textbox_title.Text;
@@ -173,29 +202,85 @@ namespace Sunrise.MultipleChoice
         }
         private void header()
         {
-            // Draw a line below the text
-            gfx.DrawLine(XPens.Black, 0, 10, page.Width, page.Height);
 
+            int x_margin = 40;
+            int y_margin = 40;
+            int y_next_step =0;
             // Draw a line above the text
-            gfx.DrawLine(XPens.Black, 0, 20, page.Width, page.Height);
+            gfx.DrawLine(XPens.Black, x_margin, y_margin, page.Width-x_margin, y_margin);
+            //Write title.
+            XRect xrt = new XRect(x_margin, y_next_step=y_margin+10, page.Width-x_margin, Convert.ToDouble(Export_to_pdf_Header_comobox_fontsize.Text));
+            gfx.DrawString(Export_to_pdf_Header_textbox.Text, header_font, XBrushes.Black, xrt, XStringFormats.Center);
+            //Draw a line below text
+            gfx.DrawLine(XPens.Black, x_margin, y_next_step + Convert.ToDouble(Export_to_pdf_Header_comobox_fontsize.Text), page.Width-x_margin, y_next_step + Convert.ToDouble(Export_to_pdf_Header_comobox_fontsize.Text));
+            //Name of the proffessor
+            xrt.X = x_margin;
+            xrt.Y = y_next_step + 10 + Convert.ToDouble(Export_to_pdf_Header_comobox_fontsize.Text);
+            //xrt.Width = page.Width - x_margin;
+            xrt.Width = page.Width*0.8;
+            xrt.Height = 10+ Convert.ToDouble(Export_to_pdf_comobox_fontsize.Text) ;
+            gfx.DrawString(Export_to_pdf_Header_textbox_nameproffesor.Text, font, XBrushes.Blue, xrt, XStringFormats.TopLeft);
+
+            xrt.X = page.Width*0.80;
+            xrt.Width = page.Width*0.20;
+            xrt.Height = 10 + Convert.ToDouble(Export_to_pdf_comobox_fontsize.Text);
+            gfx.DrawString(Export_to_pdf_datepicker.Text,font, XBrushes.Black, xrt, XStringFormats.TopLeft);
+
+            //take the values to know where the questions should start printing.
+            x = x_margin;
+            y = y_next_step + 30 + Convert.ToDouble(Export_to_pdf_Header_comobox_fontsize.Text); ;
+
         }
         private void insert_questions(String question)
         {
-            string text1 = "asdfasdfasdfasdfasdfsadfasdfasdfasdfasdfasdfasdfasdfasdfasdfsdfsadf";
-            string text2 = "sdfafasdfasdfasdfdasfffffffffffffffffffffffasdfasdfasdf";
+            //string text1 = "asdfasdfasdfasdfasdfsadfasdfasdfasdfasdfasdfasdfasdfasdfasdfsdfsadf";
+            //string text2 = "sdfafasdfasdfasdfdasfffffffffffffffffffffffasdfasdfasdf";
             //XRect xrt = new XRect(5, 5, page.Width, page.Height);
-            double y = 20;
-            XRect xrt = new XRect(20, 0, page.Width, page.Height);
-            for (int i=0;i<50;i++)
+            double yh = 20;//Space between question
+
+            bool first_page=false;
+            XRect xrt = new XRect(x, y, page.Width, page.Height);
+
+            for (int i=0;i<100;i++)
             {
                 gfx.DrawString(" "+i.ToString(), font, XBrushes.Black, xrt, XStringFormats.TopLeft);
-                xrt.Y += y;
-                if (i%30==0&& i==0)
+                xrt.Y += yh;
+                if (i%35==0 && i!=0 && first_page==false)
                 {
                     page = document.AddPage();
                     gfx = XGraphics.FromPdfPage(page);
-                    xrt.Y = 0;
+                    //xrt.Y = 0;
+                    xrt.X = 40;xrt.Y = 40;
+                    first_page = true;
                 }
+                else if (first_page == true && i % 75 == 0 &&i!=35)
+                {
+                    page = document.AddPage();
+                    gfx = XGraphics.FromPdfPage(page);
+                    xrt.X = 40; xrt.Y = 40;
+                }
+            }
+        }
+        private void dispose_objects_load()
+        {
+            document = new PdfDocument();
+            page = document.AddPage();
+            gfx = XGraphics.FromPdfPage(page);
+            //Set by default values.
+            font = new XFont("Arial", 12, XFontStyle.Regular);
+            header_font = new XFont("Arial", 12, XFontStyle.Regular);
+            page.Size = PageSize.A4;
+            page.Orientation = PageOrientation.Portrait;
+        }
+        private void initialize_parameters_file()
+        {
+            if (Export_to_pdf__textbox_filename.Text == "")
+            {
+                filename = "HelloWorld.pdf";
+            }
+            else
+            {
+                filename = Export_to_pdf__textbox_filename.Text + ".pdf";
             }
         }
     }
