00c60c81d408ee1c64589734f5c6bc8c59b7cf88
CostasDev <costask.dev@gmail.com>
2016-01-10 14:33:13 -0800

    Authentication Comments Added

--- Sunrise.MultipleChoice/Sunrise.MultipleChoice/Model/CurrentUserInfo.cs
+++ Sunrise.MultipleChoice/Sunrise.MultipleChoice/Model/CurrentUserInfo.cs
@@ -8,16 +8,22 @@ namespace Quastionnaire.Model
 {
     public class CurrentUserInfo
     {
-        public static Account CURENT_ACCOUNT = new Account() {Id = 1, Username = "questionaire", Password = "6979480030" };
 
+        //Connection
         public static string HOSTNAME = "127.0.0.1";
         public static string PORT = "3306";
         public static string DATABASE = "questionaire";
+
+
+        //When the user Logged in we use username-password for the connection
+        public static int ID = 1;
         public static string USERNAME = "questionaire";
         public static string PASSWORD = "6979480030";
+        public static Account CURENT_ACCOUNT = new Account() {Id = ID, Username = USERNAME, Password = PASSWORD };
 
+        //Temp Username - Password only For Register
         public static string TEMP_REGISTER_USERNAME = "root";
         public static string TEMP_REGISTER_PASSWORD = "123456";
-       
+
     }
 }
